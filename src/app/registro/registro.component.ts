4cda2a4ccc1fcf225930d0eaca8644b8710077da
gerardo vazquez <GerardoVazGa>
2023-12-07 15:34:17 -0600

    creacion dea archivos json

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,14 +1,12 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrl: './registro.component.css'
+  styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
-  @Output() loginEvent = new EventEmitter<boolean>();
-
+  // Declaración de tus variables
   username: string = '';
   password: string = '';
   loginFailed: boolean = false;
@@ -16,22 +14,85 @@ export class RegistroComponent {
   apellido2: string = '';
   correo: string = '';
 
+  enviarSolicitudHTTP(): void {
+    const url: string = 'http://172.16.114.96:5000/crear_archivo';
 
-  constructor(private cookieService: CookieService) { }
+    // Datos a enviar en el cuerpo de la solicitud
+    const datos = {
+      nombre_archivo: 'usuarios.json',
+      contenido_archivo: {
+        nombre: this.username,
+        apellido1: this.apellido1,
+        apellido2: this.apellido2,
+        correo: this.correo
+      }
+    };
 
-  verificarUsuario() {
-    if (this.username === 'admin' && this.password === 'admin') {
-      const token = 'ensesion';
-      this.cookieService.set('authToken', token);
-      this.loginEvent.emit(true);
-    } else {
-      this.username = '';
-      this.password = '';
-      this.loginFailed = true;
+    // Configurar la solicitud
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error al crear el archivo. Código de respuesta: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.mensaje); // Manejar la respuesta del servidor
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
 
-      setTimeout(() => {
-        this.loginFailed = false;
-      }, 1000);
-    }
+  enviarSolicitudHTTPAuth(): void {
+    const url: string = 'http://172.16.114.96:5000/crear_archivo';
+
+    // Datos a enviar en el cuerpo de la solicitud
+    const datos = {
+      nombre_archivo: 'auth.json',
+      contenido_archivo: {
+        correo: this.correo, 
+        contra: this.password
+      }
+    };
+
+    // Configurar la solicitud
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datos)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Error al crear el archivo. Código de respuesta: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data.mensaje); // Manejar la respuesta del servidor
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+  enviarDatos(): void {
+    this.enviarSolicitudHTTP();
+    this.enviarSolicitudHTTPAuth();
+    // Aquí puedes añadir más lógica si es necesaria después de enviar la solicitud
+  }
+
+  // Función para mostrar la tabla de usuarios
+  mostrarTablaUsuarios(data: any): void {
+    // Aquí se puede implementar la lógica para mostrar la tabla de usuarios
+    console.log('Mostrar tabla de usuarios:', data);
   }
 }
